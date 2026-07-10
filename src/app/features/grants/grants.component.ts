c9a558f86a3dab31979c61110bbcf157ae9d1efb
utopia-code <utopia-code.developer@gmail.com>
2026-07-10 13:52:21 +0200

    CRM frontend: Component: Grants

---
+++ src/app/features/grants/grants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grants',
+  standalone: true,
+  imports: [],
+  templateUrl: './grants.component.html',
+  styleUrl: './grants.component.css',
+})
+export class GrantsComponent {}
