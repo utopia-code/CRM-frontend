41de5d6484256ab140bc66c1f4122c99c75594ba
utopia-code <utopia-code.developer@gmail.com>
2026-07-10 13:48:30 +0200

    CRM frontend: Component: Icon

---
+++ src/app/shared/icon/icon.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.css',
+})
+export class IconComponent {
+  @Input() name = '';
+}
