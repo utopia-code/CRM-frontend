9a9ddccd791de73fc27dc6d7db7fc7fb0b7288a8
utopia-code <utopia-code.developer@gmail.com>
2026-07-20 21:59:27 +0200

    CRM frontend: Component Clients

--- src/app/features/clients/clients.component.ts
+++ src/app/features/clients/clients.component.ts
@@ -1,34 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { HeaderService } from '../../layout/components/header/services/header.service';
-import { BadgeComponent, BadgeVariant } from '../../shared/components/badge/badge.component';
+import { BadgeComponent } from '../../shared/components/badge/badge.component';
 import { EntityItemComponent } from '../../shared/components/entity-item/entity-item.component';
+import { IconComponent } from '../../shared/components/icon/icon.component';
+import { ConfirmModalComponent } from '../../shared/components/modal/confirm-modal/confirm-modal.component';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+import { CLIENT_STATUS_BADGE } from '../../shared/constants/client-status.badge.constant';
 import { ClientStatusPipe } from '../../shared/pipes/client-status.pipe';
-import { ClientStatus } from './enums/clientStatus.enum';
-import { ClientList } from './models/client-list.dto';
+import { CreateFormComponent } from './actions/form/form.component';
+import { ClientList } from './models/client-list.model';
+import { Client } from './models/client.model';
 import { ClientsService } from './services/clients.service';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
-  imports: [CommonModule, ClientStatusPipe, BadgeComponent, EntityItemComponent],
+  imports: [
+    CommonModule,
+    ClientStatusPipe,
+    BadgeComponent,
+    EntityItemComponent,
+    IconComponent,
+    ModalComponent,
+    CreateFormComponent,
+    RouterLink,
+    ConfirmModalComponent,
+  ],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css',
 })
 export class ClientsComponent implements OnInit {
-  private header = inject(HeaderService);
+  selectedClient: Client | null = null;
+  showClientModal = false;
 
+  selectedDeleteClient: ClientList | null = null;
+  showDeleteModal = false;
+
+  private header = inject(HeaderService);
+  private router = inject(Router);
+
+  /* Load service of clients list */
+  private clientsService = inject(ClientsService);
   clientsList: ClientList[] = [];
 
-  readonly clientStatusBadge: Record<ClientStatus, BadgeVariant> = {
-    [ClientStatus.POTENTIAL]: 'bg',
-    [ClientStatus.ACTIVE]: 'active',
-    [ClientStatus.INACTIVE]: 'inactive',
-  };
+  /* Set colors of badge status */
+  readonly clientStatusBadge = CLIENT_STATUS_BADGE;
 
-  private clientsService = inject(ClientsService);
-
+  /* Init view clients list */
   ngOnInit(): void {
     this.header.set({
       title: 'Clientes',
@@ -36,7 +57,7 @@ export class ClientsComponent implements OnInit {
         {
           label: 'Nuevo cliente',
           icon: 'plus',
-          action: () => this.createClient(),
+          action: () => this.openCreateModal(),
         },
       ],
     });
@@ -55,7 +76,65 @@ export class ClientsComponent implements OnInit {
     });
   }
 
-  createClient() {
-    console.log('create client');
+  getMainContact(client: ClientList) {
+    return client.contacts?.[0];
+  }
+
+  /* Open and close modal */
+  openCreateModal() {
+    this.selectedClient = null;
+    this.showClientModal = true;
+  }
+
+  /* Edit client */
+  editClient(id: number): void {
+    this.clientsService.getClient(id).subscribe({
+      next: (client) => {
+        this.selectedClient = client;
+        this.showClientModal = true;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  closeModal() {
+    this.showClientModal = false;
+    this.showClientModal = false;
+  }
+
+  onClientSaved() {
+    this.closeModal();
+    this.loadClientsList();
+  }
+
+  /* Delete client */
+  openDeleteModal(client: ClientList): void {
+    this.selectedDeleteClient = client;
+    this.showDeleteModal = true;
+  }
+
+  confirmDeleteClient() {
+    if (!this.selectedDeleteClient) {
+      return;
+    }
+
+    const clientId = this.selectedDeleteClient.id;
+
+    this.clientsService.removeClient(clientId).subscribe({
+      next: () => {
+        // Update list
+        this.clientsList = this.clientsList.filter((item) => item.id !== clientId);
+
+        this.closeDeleteModal();
+      },
+      error: console.error,
+    });
+  }
+
+  closeDeleteModal(): void {
+    this.showDeleteModal = false;
+    this.selectedDeleteClient = null;
   }
 }
