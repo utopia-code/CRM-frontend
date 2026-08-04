bf7bae4b01c27ba5d6af005a6bb41bb3e3c0081f
utopia-code <utopia-code.developer@gmail.com>
2026-08-04 11:42:43 +0200

    CRM frontend: Footer: set export and import buttons

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,13 +1,58 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BackupService } from '../../../core/services/backup.service';
+import { IconComponent } from '../../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  selectedFile?: File;
 
+  private readonly backupService = inject(BackupService);
+
+  exportBackup() {
+    this.backupService.exportBackup().subscribe((blob) => {
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+
+      link.href = url;
+
+      link.download = `backup-${new Date().toISOString()}.json`;
+
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+    });
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length) {
+      this.selectedFile = input.files[0];
+      this.restoreBackup();
+    }
+  }
+
+  restoreBackup() {
+    if (!this.selectedFile) {
+      return;
+    }
+
+    this.backupService.restoreBackup(this.selectedFile).subscribe({
+      next: (response) => {
+        console.log('Backup restaurado:', response);
+      },
+
+      error: (error) => {
+        console.error('Error restaurando backup:', error);
+      },
+    });
+  }
 }
