102be2331f468ae654d954f42401111f4f2d6242
utopia-code <utopia-code.developer@gmail.com>
2026-08-06 09:25:01 +0200

    CRM frontend: set locale lang GL

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+import { registerLocaleData } from '@angular/common';
+import localeGl from '@angular/common/locales/gl';
+import { ApplicationModule } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
+registerLocaleData(localeGl);
+
+platformBrowserDynamic()
+  .bootstrapModule(ApplicationModule)
   .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
