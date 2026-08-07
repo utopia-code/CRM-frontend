7f00281bc8376258c9a8690a23f02cc5d78869ca
utopia-code <utopia-code.developer@gmail.com>
2026-08-07 12:49:41 +0200

    CRM frontend: maint.ts: delete bootstrap module

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { registerLocaleData } from '@angular/common';
 import localeGl from '@angular/common/locales/gl';
-import { ApplicationModule } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 registerLocaleData(localeGl);
-
-platformBrowserDynamic()
-  .bootstrapModule(ApplicationModule)
-  .catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
